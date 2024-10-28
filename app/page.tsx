7046a649e00c4a304376697a4cc2b2bb2e01a658
Codngwthubaid <codngwthubaid@gmail.com>
2024-10-28 19:11:59 +0530

    Add ShadCN components and working on Banner components

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import Banner from "@/components/home/Banner";
+
 export default function Home() {
   return (
   <>
-  <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus blanditiis labore mollitia, natus nemo ipsum harum culpa temporibus maxime repellat eaque officia! Modi autem labore impedit, reprehenderit ab at ratione.</div>
+  <article>
+    <Banner/>
+  </article>
   </>
   );
 }
--- components/home/Banner.tsx
+++ components/home/Banner.tsx
@@ -1,11 +1,22 @@
 import React from 'react'
+import { Button } from "@/components/ui/button"
+import Link from 'next/link'
 
 const Banner = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <section className='lg:max-w-6xl mx-auto flex flex-col z-0 items-center justify-center py-28 sm:pt-32 transition-all animate-in'>
+            <h1 className='py-6 text-center'>Turn your words into {" "}
+                <span className='underline underline-offset-8 decoration-dashed decoration-purple-400'>Captivating</span> {" "} blog posts
+            </h1>
+            <h2 className='text-center px-4 lg:px-0 lg:max-w-4xl'>Convert your videos and voice into Blog Post in seconds with the power of AI
+            </h2>
+
+            <Button variant={"link"} className=''>
+                <Link href="/#pricing">Get's NUVN with the power of AI</Link>
+            </Button>
+
+        </section>
+    )
 }
 
 export default Banner
