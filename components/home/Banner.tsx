ea76a8aeaba3191e4258987fcfdce199c2106573
Codngwthubaid <codngwthubaid@gmail.com>
2024-10-29 07:32:57 +0530

    Banner Section is completes, Now start working on the Header Section

--- components/home/Banner.tsx
+++ components/home/Banner.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Button } from "@/components/ui/button"
 import Link from 'next/link'
+import { ArrowRight } from 'lucide-react'
 
 const Banner = () => {
     return (
@@ -11,8 +12,11 @@ const Banner = () => {
             <h2 className='text-center px-4 lg:px-0 lg:max-w-4xl'>Convert your videos and voice into Blog Post in seconds with the power of AI
             </h2>
 
-            <Button variant={"link"} className=''>
-                <Link href="/#pricing">Get's NUVN with the power of AI</Link>
+            <Button variant={"link"} className='mt-6 text-xl px-12 rounded-full py-8 lg:mt-20 bg-gradient-to-r from-purple-600  to-indigo-600 hover:from-indigo-600 hover:to-purple-600 text-white font-bold shadow-lg hover:no-underline'>
+                <Link href="/#pricing" className='flex gap-x-3 items-center'>
+                <span className='relative'>Get's NUVN with the power of AI</span>
+                <ArrowRight className='animate-pulse'/>
+                </Link>
             </Button>
 
         </section>
