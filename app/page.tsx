b4de30d7633b50bcb0db8c98f767ea7b11e21ca1
Codngwthubaid <codngwthubaid@gmail.com>
2024-10-29 08:30:58 +0530

    Header and Banner Section Completes and started working on the HoeItWorks section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,19 @@
 import Banner from "@/components/home/Banner";
+import { Dot } from "lucide-react";
 
 export default function Home() {
   return (
-  <>
-  <article>
-    <Banner/>
-  </article>
-  </>
+    <>
+      <main>
+        <div>
+          <Banner />
+        </div>
+        <div className="flex items-center justify-center">
+          <Dot className="text-purple-400" />
+          <Dot className="text-purple-400" />
+          <Dot className="text-purple-400" />
+        </div>
+      </main>
+    </>
   );
 }
--- components/home/Header.tsx
+++ components/home/Header.tsx
@@ -1,11 +1,38 @@
 import React from 'react'
+import Link from 'next/link'
+import { Signature } from 'lucide-react'
 
-const Header = () => {
-  return (
-    <div>
-      Im header
-    </div>
-  )
+const Navlink = ({ href, children }: { href: string; children: React.ReactNode }) => {
+    return (
+        <Link href={href} className='transition-colors duration-200 text-gray-600 hover:text-purple-500 flex items-center gap-2'>
+            {children}
+        </Link>
+    )
 }
 
-export default Header
+export default function Header() {
+    return (
+        <nav className='container flex justify-between items-center px-8 py-4 mx-auto'>
+            <div className='flex lg:flex-1'>
+                <Navlink href="/">
+                    <span className='flex items-center gap-2 shrink-0'>
+                        <Signature className='hover:rotate-12 transform transition duration-200 ease-in-out' />
+                    </span>
+                    <span className='font-extrabold text-lg'>NU-VideoNarrative</span>
+                </Navlink>
+            </div>
+            <div className='flex lg:justify-center gap-2 lg:gap-12 lg:items-center'>
+                <Navlink href="/#pricing">Pricing</Navlink>
+                <Navlink href="/#posts">Your Post</Navlink>
+            </div>
+            <div className='flex lg:flex-1 lg:justify-end gap-2'>
+                <div className='flex gap-2 items-center'>
+                    <Navlink href='/dashboard'>Upload a Video</Navlink>
+                </div>
+                <Navlink href='/sign-in'>Sign In</Navlink>
+            </div>
+        </nav>
+    )
+}
+
+
